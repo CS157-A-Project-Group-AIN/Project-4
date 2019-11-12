42fb887be985f8870bbc93e0152c9741fc864874
Nathaniel Boas <nbboas@gmail.com>
2019-11-11 23:17:52 -0800

    Created Config Folder and Handlers Class

---
+++ src/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    public static String USERNAME = "example";
+    public static String PASSWORD = "example";
+    public static String DB_URL = "example";
+    public static String JDBC_DRIVER = "example";
+}
