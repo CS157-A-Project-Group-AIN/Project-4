061b1e7c8f14732ecd87edb8d68a6f8b33f7a6a6
Nathaniel Boas <nbboas@gmail.com>
2019-11-11 23:36:40 -0800

    Created Package for Handlers

---
+++ src/Handlers/Handlers.java
@@ -0,0 +1,9 @@
+package Handlers;
+
+public class Handlers {
+public static AudiologyHandler audiologyHandler = new AudiologyHandler();
+public static ChemicalRefHandler chemicalRefHandler = new ChemicalRefHandler();
+public static DiseaseRefHandler diseaseRefHandler = new DiseaseRefHandler();
+public static MedicamentRefHandler medicamentRefHandler = new MedicamentRefHandler();
+public static PharmocologyHandler pharmocologyHandler = new PharmocologyHandler();
+}
