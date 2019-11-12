3d5d8139eface15e0687a6898be1f2a2fe35d830
Andres C <andrescortes122@yahoo.com>
2019-11-11 22:26:11 -0800

    ETRT Driver layouts

--- src/ETRTDriver.java
+++ src/ETRTDriver.java
@@ -1,8 +1,9 @@
 import java.awt.CardLayout;
-import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,13 +17,25 @@ public class ETRTDriver {
     JButton buttonOne = new JButton("Switch to second panel/workspace");
     JButton buttonSecond = new JButton("Switch to first panel/workspace");
     CardLayout layout = new CardLayout();
+    GridLayout gLayout = new GridLayout();
 
     public ETRTDriver() {
-    	frame.setSize(800, 600);
+    	frame.setSize(600, 400);
         
         
         panelFirst.add(buttonOne);
+        panelFirst.add(new JButton("op2"));
+        panelFirst.add(new JButton("op3"));
+        panelFirst.add(new JButton("op4"));
+        gLayout.setRows(2);
+        gLayout.setColumns(2);
+        gLayout.setHgap(40);
+        gLayout.setVgap(40);
+        panelFirst.setLayout(gLayout);
+        panelFirst.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+        
         panelSecond.add(buttonSecond);
+        
         
         panelContainer.setLayout(layout);
         panelContainer.add(panelFirst, "1");
